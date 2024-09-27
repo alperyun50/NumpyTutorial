683fc31b84beba91d2bd45043c6d285975e9e5bf
alperyun50 <yavuzun50@hotmail.com>
2024-09-28 00:02:00 +0300

    arange

--- main.py
+++ main.py
@@ -21,3 +21,24 @@ print(a_mul.shape)
 print(a_mul.ndim)
 print(a_mul.size)
 print(a_mul.dtype)
+
+
+
+
+ab = np.full((2,3,4), 9)
+print(ab)
+
+
+ab = np.zeros((10,5,2))
+print(ab)
+
+ab = np.ones((10,5,2))
+print(ab)
+
+ab = np.empty((5,5,5))
+print(ab)
+
+
+
+x_values = np.arange(0, 1000, 5)
+print(x_values)
