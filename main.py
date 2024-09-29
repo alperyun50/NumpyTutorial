22c06ac98781190dd2b0c86f041e6f943ab301c5
alperyun50 <yavuzun50@hotmail.com>
2024-09-29 19:54:43 +0300

    append

--- main.py
+++ main.py
@@ -42,3 +42,40 @@ print(ab)
 
 x_values = np.arange(0, 1000, 5)
 print(x_values)
+
+x_values = np.linspace(0, 1000, 4)
+print(x_values)
+
+
+print(np.nan)
+print(np.inf)
+
+print(np.isnan(np.nan))
+
+
+l1 = [1,2,3,4,5]
+l2 = [6,7,8,9,0]
+
+a1 = np.array(l1)
+a2 = np.array(l2)
+
+print(l1 * 5)
+print(a1 * 5)
+
+
+a1 = np.array([1,2,3])
+a2 = np.array([[1], [2]])
+
+print(a1 + a2)
+
+
+a = np.array([[1,2,3], 
+              [4,5,5]])
+
+print(np.log10(a))
+
+
+a = np.array([1,2,3])
+a = np.append(a, [7,8,9])
+
+print(a)
